64a5a862f7524c9be892bf6e672c9d16cb716b76
T_K <sidepeng@163.com>
2018-11-12 15:34:50 +0800

    date ago

--- jsUtils/ago.js
+++ jsUtils/ago.js
@@ -3,12 +3,12 @@ Date.prototype.__defineGetter__('ago', function () {
     var diff = (new Date().getTime() - this.getTime()) / 1000,
         day_diff = Math.floor(diff / 86400);
     return day_diff == 0 && (
-            diff < 60 && "just now" ||
-            diff < 120 && "1 minute age" ||
-            diff < 3600 && Math.floor(diff / 60) + 'minutes age' ||
-            diff < 7200 && "1 hour ago" ||
-            diff < 86400 && Math.floor(diff / 3600) + "hours ago") ||
-        day_diff == 1 && "Yesterday" ||
-        day_diff < 7 && day_diff + 'days ago' ||
-        Math.ceil(day_diff / 7) + "weeks ago";
+            diff < 60 && "刚刚" ||
+            diff < 120 && "1 分钟之前" ||
+            diff < 3600 && Math.floor(diff / 60) + ' 分钟之前' ||
+            diff < 7200 && "1 小时之前" ||
+            diff < 86400 && Math.floor(diff / 3600) + " 小时之前") ||
+        day_diff == 1 && "昨天" ||
+        day_diff < 7 && day_diff + ' 天前' ||
+        Math.ceil(day_diff / 7) + " 周前";
 })
